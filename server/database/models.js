ff453ece64d49e10d521b695427380d51d0a9903
ModeuSProgrammer <niffty999@gmail.com>
2024-02-14 22:49:16 +0300

    0.0.0.5v
    Создали базу данных + схему, проверили связи, сделали разметку роутев, подняли снова сервак, сделали контроллеры место для них

--- server/database/models.js
+++ server/database/models.js
@@ -1,2 +1,110 @@
 const sequelize = require('./db.js')  // база данных
 const { DataTypes } = require('sequelize') // для использование типов полей
+
+const User = sequelize.define('User', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+},
+  {
+    timestamps: false,
+  })
+
+const Role = sequelize.define('Role', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, unique: true }
+},
+  {
+    timestamps: false,
+  })
+
+const Menu = sequelize.define('Menu', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  date: { type: DataTypes.DATE },
+  chef: { type: DataTypes.STRING },
+},
+  {
+    timestamps: false
+  })
+
+const First = sequelize.define('First', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, unique: true },
+  weight: { type: DataTypes.STRING, }
+},
+  {
+    timestamps: false
+  })
+
+const Second = sequelize.define('Second', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, unique: true },
+  weight: { type: DataTypes.STRING, }
+},
+  {
+    timestamps: false
+  })
+
+const Dessert = sequelize.define('Dessert', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, unique: true },
+  weight: { type: DataTypes.STRING, }
+},
+  {
+    timestamps: false
+  })
+
+const Drink = sequelize.define('Drink', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, unique: true },
+  weight: { type: DataTypes.STRING, }
+},
+  {
+    timestamps: false
+  })
+
+const Additional = sequelize.define('Additional', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, unique: true },
+  weight: { type: DataTypes.STRING, }
+},
+  {
+    timestamps: false
+  })
+
+const Comment = sequelize.define('Comment', {
+  ID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  text: { type: DataTypes.STRING },
+  date: { type: DataTypes.DATE, },
+},
+  {
+    timestamps: false
+  })
+
+Role.hasOne(User)
+User.belongsTo(Role)
+
+Menu.hasMany(User)
+User.belongsTo(Menu)
+
+First.hasOne(Menu)
+Menu.belongsTo(First)
+
+Second.hasOne(Menu)
+Menu.belongsTo(Second)
+
+Dessert.hasOne(Menu)
+Menu.belongsTo(Dessert)
+
+Drink.hasOne(Menu)
+Menu.belongsTo(Drink)
+
+Menu.hasMany(Additional)
+Additional.belongsTo(Menu)
+
+Menu.hasMany(Comment)
+Comment.belongsTo(Menu)
+
+module.exports = {
+  User, Role, Menu, Drink, Dessert, Comment, Additional, Second, First
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express') // import express
-
-const sequelize = require('./database/db.js') // add on DB
-const models = require('./database/models.js') // add on 
 const dotenv = require('dotenv')// file config
 dotenv.config() // settings variables
 
+const sequelize = require('./database/db.js') // add on DB
+const models = require('./database/models.js') // add on 
+
 const app = express() // create express app
+
+
 const port = process.env.PORT || 5500
 
 
